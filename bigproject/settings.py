6bfcfff5c010d5cf3bd40ce0466c40d39701c61b
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-02-21 17:42:28 -0300

    Fixed issues

--- bigproject/settings.py
+++ bigproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['immense-hollows-40437.herokuapp.com']
 
 
 # Application definition
