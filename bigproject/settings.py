fe10e1fc7a08e2244752d574de7cdb2d074a6cf6
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-03-23 16:09:02 -0300

    Pagination settings added

--- bigproject/settings.py
+++ bigproject/settings.py
@@ -149,6 +149,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Add Pagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
