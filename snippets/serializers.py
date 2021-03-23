1703fbb22b69b98ace8a84884a4efb40fe3ecdf1
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-03-23 15:52:23 -0300

    Snippets created

---
+++ snippets/serializers.py
@@ -0,0 +1,24 @@
+from snippets.models import Snippet
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    # Can use ChartField(read_only=True) instead of ReadOnlyField
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'highlight', 'title', 'code', 'linenos',
+                  'language', 'style', 'owner']
