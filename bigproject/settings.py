11717803756215c09f981f71780b545f4c5fca11
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-03-26 11:40:55 -0300

    Without pagination

--- bigproject/settings.py
+++ bigproject/settings.py
@@ -158,10 +158,10 @@ USE_TZ = True
 
 
 # Add Pagination
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10
+# }
 
 
 CORS_ORIGIN_WHITELIST = ['https://localhost', 'https://127.0.0.1']
