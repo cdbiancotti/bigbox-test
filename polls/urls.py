3749b7e3b681d89040d4c6951901236b1b818845
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-04-04 05:43:24 -0300

    Some tests were added

--- polls/urls.py
+++ polls/urls.py
@@ -5,8 +5,10 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-    path(r'', views.index, name='index'),
-    path(r'<int:question_id>', views.detail, name='detail'),
-    path(r'<int:question_id>/results/', views.results, name='results'),
+    path(r'', views.IndexView.as_view(), name='index'),
+    # generic.DetailView expects the primary key value captured from the URL to be called "pk"
+    # that's the reason why we've changed "question_id" to "pk"
+    path(r'<int:pk>', views.DetailView.as_view(), name='detail'),
+    path(r'<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path(r'<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,36 @@
-from django.http.response import Http404, HttpResponseRedirect
+from django.http.response import HttpResponseRedirect
 from polls.models import Choice, Question
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
 from django.urls import reverse
-from django.template import loader
+from django.views import generic
+from django.utils import timezone
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    # generic.ListView by default uses <app name>/<model name>_list .html like template_name
+    template_name = 'polls/index.html'
+    # generic.ListView by default uses <model name>_list like context_object_name
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    # generic.DetailView by default uses <app name>/<model name>_detail.html like template_name
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
